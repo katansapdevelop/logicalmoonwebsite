26d2f35d7914eaa183a485ac674a8e09643f26e1
Rory <rory@Air-II.local>
2021-11-21 21:30:39 +1000

    add cypress action 3

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,7 +15,7 @@ module.exports = (on, config) => {
     fixturesFolder: 'cypress/fixtures',
     integrationFolder: 'cypress/specs',
     screenshotsFolder: 'cypress/screenshots',
-    videosFolder: 'cypress/videos',
+    // videosFolder: 'cypress/videos',
     supportFile: 'cypress/support/index.ts',
   })
 }
